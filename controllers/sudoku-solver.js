e0e775315128383a518cca7f1ef7cc1fc68da7e7
luiggy macias <ferrinluiggy@gmail.com>
2022-01-03 14:57:06 -0500

    api check

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,11 +1,70 @@
 class SudokuSolver {
   validate(puzzleString) {}
+  letterToNumber(row) {
+    switch (row.toUpperCase()) {
+      case "A":
+        return 1;
+      case "B":
+        return 2;
+      case "C":
+        return 3;
+      case "D":
+        return 4;
+      case "E":
+        return 5;
+      case "F":
+        return 6;
+      case "G":
+        return 7;
+      case "H":
+        return 8;
+      case "I":
+        return 9;
+      default:
+        return "none";
+    }
+  }
+  checkRowPlacement(puzzleString, row, column, value) {
+    let grid = this.transform(puzzleString);
+    row = this.letterToNumber(row);
+    if (grid[row - 1][column - 1] !== 0) {
+      return false;
+    }
+    for (let i = 0; i < 9; i++) {
+      if (grid[row - 1][i] == value) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-  checkRowPlacement(puzzleString, row, column, value) {}
+  checkColPlacement(puzzleString, row, column, value) {
+    let grid = this.transform(puzzleString);
+    row = this.letterToNumber(row);
+    if (grid[row - 1][column - 1] !== 0) {
+      return false;
+    }
+    for (let i = 0; i < 9; i++) {
+      if (grid[i][column - 1] == value) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-  checkColPlacement(puzzleString, row, column, value) {}
-
-  checkRegionPlacement(puzzleString, row, column, value) {}
+  checkRegionPlacement(puzzleString, row, col, value) {
+    let grid = this.transform(puzzleString);
+    row = this.letterToNumber(row);
+    if (grid[row - 1][col - 1] !== 0) {
+      return false;
+    }
+    let startRow = row - (row % 3),
+      startCol = col - (col % 3);
+    for (let i = 0; i < 3; i++)
+      for (let j = 0; j < 3; j++)
+        if (grid[startRow + i][startCol + j] == value) return false;
+    return true;
+  }
   solveSodoku(grid, row, col) {
     if (row == 9 - 1 && col == 9) return grid;
     if (col == 9) {
--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,53 @@ module.exports = function (app) {
   let solver = new SudokuSolver();
 
   app.route("/api/check").post((req, res) => {
-    console.log("req.body :>> ", req.body);
+    const { puzzle, coordinate, value } = req.body;
+    if (!puzzle || !coordinate || !value)
+      return res.status(400).send("Invalid request");
+    const row = coordinate.split("")[0];
+    const column = coordinate.split("")[1];
+    if (
+      coordinate.length !== 2 ||
+      !/[a-i]/i.test(row) ||
+      !/[0-9]/i.test(column)
+    ) {
+      console.log("invalid coordinate :>> ", coordinate);
+      return res.json({ error: "invalid coordinate" });
+    }
+    if (!/[1-9]/i.test(value)) {
+      console.log("invalid value :>> ", value);
+      return res.json({ error: "invalid value" });
+    }
+    if (puzzle.length !== 81) {
+      console.log("invalid puzzle :>> ", puzzle);
+      return res.json({ error: "Expected puzzle to be 81 characters long" });
+    }
+    if (/[^1-9.]/g.test(puzzle)) {
+      console.log("invalid puzzle :>> ", puzzle);
+      return res.json({ error: "Invalid characters in puzzle" });
+    }
+    let validCol = solver.checkColPlacement(puzzle, row, column, value);
+    let validReg = solver.checkRegionPlacement(puzzle, row, column, value);
+    let validRow = solver.checkRowPlacement(puzzle, row, column, value);
+    let conflicts = [];
+    if (validCol && validReg && validRow) {
+      return res.json({ valid: true });
+    } else {
+      if (!validCol) {
+        conflicts.push("column");
+      }
+      if (!validReg) {
+        conflicts.push("region");
+      }
+      if (!validRow) {
+        conflicts.push("row");
+      }
+      return res.json({ valid: false, conflicts });
+    }
+  });
+
+  app.route("/api/solve").post((req, res) => {
+    // console.log("req.body :>> ", req.body);
     const { puzzle } = req.body;
     if (!puzzle) return res.json({ error: "Required field missing" });
     if (puzzle.length != 81)
@@ -17,8 +63,6 @@ module.exports = function (app) {
     if (!solvedString) return res.json({ error: "Puzzle cannot be solved" });
     return res.json({ solution: solvedString });
   });
-
-  app.route("/api/solve").post((req, res) => {});
 };
 
 //
