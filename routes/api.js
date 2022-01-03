a424cff800a9f13cf0fc30bdd2612beabf10e046
luiggy macias <ferrinluiggy@gmail.com>
2022-01-03 11:46:25 -0500

    doing /api/check

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,21 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {
+    const { puzzle } = req.body;
+    if (!puzzle) return res.json({ error: "Required field missing" });
+    if (puzzle.length != 81)
+      return res.json({ error: "Expected puzzle to be 81 characters long" });
+    if (/[^0-9.]/g.test(puzzle))
+      return res.json({ error: "Invalid characters in puzzle" });
+    let solvedString = solver.solve(puzzle);
+    if (!solvedString) return res.json({ error: "Puzzle cannot be solved" });
+    return res.json({ solution: solvedString });
+  });
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
-
-    });
+  app.route("/api/solve").post((req, res) => {});
 };
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,50 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
+const Solver = require("../controllers/sudoku-solver.js");
 let solver;
 
-suite('UnitTests', () => {
-
+suite("UnitTests", () => {
+  test("should ", (done) => {
+    done();
+  });
+  test("should ", (done) => {
+    done();
+  });
+  test("should ", (done) => {
+    done();
+  });
+  test("should ", (done) => {
+    done();
+  });
+  test("should ", (done) => {
+    done();
+  });
+  test("should ", (done) => {
+    done();
+  });
+  test("should ", (done) => {
+    done();
+  });
+  test("should ", (done) => {
+    done();
+  });
+  test("should ", (done) => {
+    done();
+  });
+  test("should ", (done) => {
+    done();
+  });
+  test("should ", (done) => {
+    done();
+  });
+  test("should ", (done) => {
+    done();
+  });
+  test("should ", (done) => {
+    done();
+  });
+  test("should ", (done) => {
+    done();
+  });
 });
